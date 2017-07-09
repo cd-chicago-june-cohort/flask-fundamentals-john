96f90242b265f981ea5d811fc95c9cf95a46f4d5
JP Doherty <jpdoherty90@gmail.com>
2017-07-09 16:56:35 -0500

    most of ninja gold finished, will return to later

---
+++ ninja_gold/server.py
@@ -0,0 +1,38 @@
+from flask import Flask, render_template, redirect, request, session
+from random import randint
+
+app = Flask(__name__)
+app.secret_key = "0923485720934578"
+
+session = {
+    "score": 0,
+    "message": ""
+}
+
+@app.route("/")
+def index():
+    score = session["score"]
+    msg = session["message"]
+    return render_template("index.html", ninja_score=score, my_message=msg)
+
+@app.route("/process_money", methods=["POST"])
+def process_money():
+    
+    if request.form["building"] == "farm":
+        farm_rand = randint(10, 20)
+        session["score"] += farm_rand
+        session["message"] = "Earned " + str(farm_rand) + " golds at the farm!"
+    elif request.form["building"] == "cave":
+        cave_rand = randint(5,10)
+        session["score"] += cave_rand
+        session["message"] = "Earned " + str(cave_rand) + " golds at the farm!"
+    elif request.form["building"] == "house":
+        house_rand = randint(2, 5)
+        session["score"] += house_rand
+        session["message"] = "Earned " + str(house_rand) + " golds at the farm!"
+    elif request.form["building"] == "casino":
+        session["score"] += randint(-50, 50)
+    
+    return redirect("/")
+
+app.run(debug=True)
